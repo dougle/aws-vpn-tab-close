1fbc5d35c7286c725e0910340ce0acebc118d43a
Daniel Craig <daniel@danielcraig.me>
2023-02-18 09:25:14 +0000

    added comments and reformatted code

--- scripts/background.js
+++ scripts/background.js
@@ -1,8 +1,12 @@
+// listen for messages
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
+    function(request, sender, sendResponse) {
+    // check the message command for a close tab command
     if (request.command === "close_tab")
+        // find the currently active tab
         chrome.tabs.query({ active: true }, function(tabs) {
-             chrome.tabs.remove(tabs[0].id);
+            // close the found tab
+            chrome.tabs.remove(tabs[0].id);
         });
-  }
+    }
 );
--- scripts/content.js
+++ scripts/content.js
@@ -1,8 +1,9 @@
 var body_content = document.body.innerText;
 var auth_message = "Authentication details received, processing details. You may close this window at any time.";
 
-console.log(body_content);
+// if the body element only contains the confirmation message above
 if(body_content == auth_message){
+    // send a message to background.js to close the tab
     chrome.runtime.sendMessage({
         command: "close_tab"
     });
