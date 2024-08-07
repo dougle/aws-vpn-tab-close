d7d1b246d0ead8ec9e2d75cd9d8f0dc3a1d2c43d
TwoBadRobots <daniel@danielcraig.me>
2024-08-07 13:31:23 +0100

    loop through all tabs comparing to sender to be confident that the right tab is being closed

--- scripts/background.js
+++ scripts/background.js
@@ -1,12 +1,22 @@
 // listen for messages
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
+//    console.log(sender);
     // check the message command for a close tab command
     if (request.command === "close_tab")
-        // find the currently active tab
-        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-            // close the found tab
-            chrome.tabs.remove(tabs[0].id);
+        // empty query gets all tabs from all windows
+        chrome.tabs.query({}, function(tabs) {
+//            console.log(tabs);
+            // loop through all tabs in all windows
+            for (let i = 0; i < tabs.length; i++) {
+//                console.log("comparing tab", tabs[i], sender.tab);
+                // compare the current tab to the tab that sent the message
+                if (tabs[i].id == sender.tab.id)
+                    // close the found tab
+//                    console.log("closing tab", tabs[i]);
+                    chrome.tabs.remove(tabs[i].id);
+
+            }
         });
     }
 );
