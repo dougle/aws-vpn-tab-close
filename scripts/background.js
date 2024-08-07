e90187e98fe7b4b52a4ea644153809b6bfaa1423
TwoBadRobots <daniel@danielcraig.me>
2024-08-07 14:32:18 +0100

    condense background tab closing logic down

--- scripts/background.js
+++ scripts/background.js
@@ -1,22 +1,10 @@
 // listen for messages
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-//    console.log(sender);
-    // check the message command for a close tab command
-    if (request.command === "close_tab")
-        // empty query gets all tabs from all windows
-        chrome.tabs.query({}, function(tabs) {
-//            console.log(tabs);
-            // loop through all tabs in all windows
-            for (let i = 0; i < tabs.length; i++) {
-//                console.log("comparing tab", tabs[i], sender.tab);
-                // compare the current tab to the tab that sent the message
-                if (tabs[i].id == sender.tab.id)
-                    // close the found tab
-//                    console.log("closing tab", tabs[i]);
-                    chrome.tabs.remove(tabs[i].id);
+//        console.log(request, sender);
+        // check the message command for a close tab command
+        if (request.command === "close_tab")
+            chrome.tabs.remove(sender.tab.id);
 
-            }
-        });
     }
 );
