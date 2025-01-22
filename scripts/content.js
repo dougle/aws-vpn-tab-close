7f9bf75698f76cac22c9dcaaf5add0cbe34c5540
Felix Keller <felix.keller@infotip.de>
2025-01-22 10:45:15 +0100

    added support for german

--- scripts/content.js
+++ scripts/content.js
@@ -1,8 +1,11 @@
 var body_content = document.body.innerText;
-var auth_message = "Authentication details received, processing details. You may close this window at any time.";
+var auth_messages = [
+    "Authentication details received, processing details. You may close this window at any time.",  // english
+    "Authentifizierungsdetails empfangen, Details werden verarbeitet. Sie können dieses Fenster jederzeit schließen.", // german
+];
 
 // if the body element only contains the confirmation message above
-if(body_content == auth_message){
+if(auth_messages.includes(body_content)){
     // send a message to background.js to close the tab
     chrome.runtime.sendMessage({
         command: "close_tab"
