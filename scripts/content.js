af2e1e6a63d85de452e426c9dac0a268488ca0df
Dougle <github@gigaclear.33mail.com>
2026-08-21 11:33:34 +0100

    add some comments

--- scripts/content.js
+++ scripts/content.js
@@ -1,19 +1,18 @@
 var dqs = function(sel){return document.querySelector(sel);}
 
+// check for success message, we don't want to close if there is an error to show the user
 var v5_auth_message = [
     "Authentication details received, processing details. You may close this window at any time.", // english
     "認証の詳細を受信、詳細を処理中です。このウィンドウをいつでも閉じることができます。", // japanese
     "Authentifizierungsdetails empfangen, Details werden verarbeitet. Sie können dieses Fenster jederzeit schließen.", // german
 ];
-
 var v6_auth_message = [
     "Authentication successful\n\nYou may now close this browser tab.", // english
 ];
 
-// v5 and v6 confirmation pages differ
+// v5 and v6 confirmation pages differ in structure
 var v5_auth = v5_auth_message.includes(dqs('body').innerText);
 var v6_auth = (dqs('.header .brand .aws') && v6_auth_message.includes(dqs('.card').innerText));
-
 
 // if the body element only contains the confirmation message above
 if(v5_auth || v6_auth){
