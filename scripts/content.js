b6069d825f2fd42637084fca49d322c31fb565c9
Dougle <github@gigaclear.33mail.com>
2026-08-21 10:27:01 +0100

    support client v6

--- scripts/content.js
+++ scripts/content.js
@@ -1,10 +1,13 @@
-var body_content = document.body.innerText;
-var auth_message = "Authentication details received, processing details. You may close this window at any time.";
+var dqs = function(sel){return document.querySelector(sel);}
 
-// if the body element only contains the confirmation message above
-if(body_content == auth_message){
-    // send a message to background.js to close the tab
-    chrome.runtime.sendMessage({
+// v5 and v6 confirmation pages differ
+var v5_auth = (document.body.innerText == "Authentication details received, processing details. You may close this window at any time.");
+var v6_auth = (dqs('.header .brand .aws') && dqs('.card').innerText == "Authentication successful\n\nYou may now close this browser tab.");
+
+// if the page is definitely the one we want
+if(v5_auth || v6_auth){
+    // send a message to background.js to close *this* tab
+     chrome.runtime.sendMessage({
         command: "close_tab"
     });
 }
